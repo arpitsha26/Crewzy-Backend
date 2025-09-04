abda319cae38c57abcb43a05b3aee25a3d3b66e9
arpitsha26 <arpitsharma1263@gmail.com>
2025-09-04 20:50:06 +0530

    update origin url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://crewzy-npib.onrender.com",
     credentials:true,
     
 }))
